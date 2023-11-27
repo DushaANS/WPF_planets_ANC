a409b4520281c9af202eba4e980634efd5aa74c3
Dusha_Ada <lola180880mail.ru@gmail.com>
2023-11-27 19:14:59 +0500

    //Planets

---
+++ WPF_planets_ANC/ConvertOrbit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+namespace NewListBox
+{
+    public class ConvertOrbit : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+        CultureInfo culture)
+        {
+            double orbit = (double)value;
+            double factor = System.Convert.ToDouble(parameter,
+            culture.NumberFormat); // Получаем форматирование для единиц измерения (зависит от настроек операционной системы по умолчанию, а именно от региона и региональных стандартов), parameter - получаем извне
+            return Math.Pow(orbit / 40, 0.4) * 700 * factor; // Возводим выражение за запятой в степень 0.4, необходимо для определения положения в элементе управления в пикселях? определяет значения для положения по X и Y, 700 - размер окна, factor - получаем извне(parameter)
+        }
+        public object ConvertBack(object value, Type targetType, object parameter,
+        CultureInfo culture)
+        {
+            throw new NotSupportedException("Этот метод не должен быть вызван");
+        }
+    }
+}
---
+++ WPF_planets_ANC/SolarSystemObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace NewListBox
+{
+    public class SolarSystemObject
+    {
+        // Название планеты
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        // Орбита, в астрономических единицах (а. е)
+        private double orbit;
+        public double Orbit
+        {
+            get { return orbit; }
+            set { orbit = value; }
+        }
+        // Средний диаметр, в км
+        private double diameter;
+        public double Diameter
+        {
+            get { return diameter; }
+            set { diameter = value; }
+        }
+        // Относительная ссылка на изображение
+        private Uri image;
+        public Uri Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+        // Описание
+        private string details;
+        public string Details
+        {
+            get { return details; }
+            set { details = value; }
+        }
+        // Метод для работы с коллекцией элементов
+        public SolarSystemObject(string name, double orbit, double diameter, Uri
+        image, string details)
+        {
+            this.name = name;
+            this.orbit = orbit;
+            this.diameter = diameter;
+            this.image = image;
+            this.details = details;
+        }
+        // Вместо new использован virtual-override
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
